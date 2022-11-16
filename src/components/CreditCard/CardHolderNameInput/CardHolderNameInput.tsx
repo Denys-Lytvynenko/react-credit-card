29e5a5762b980aa6fec53be2060fa8a55cc58113
Denys Lytvynenko <den.lytvynenko@temabit.com>
2022-11-16 12:09:02 +0200

    Allow only letters and spaces in card holder input

--- src/components/CreditCard/CardHolderNameInput/CardHolderNameInput.tsx
+++ src/components/CreditCard/CardHolderNameInput/CardHolderNameInput.tsx
@@ -1,11 +1,21 @@
-import { Field } from "formik";
-import { FC } from "react";
+import { Field, useField } from "formik";
+import { ChangeEvent, FC } from "react";
 
+import { onlyLetters } from "../../../utils/patterns";
 import { CardHolderNameInputType } from "./types";
 
 const CardHolderNameInput: FC<CardHolderNameInputType> = ({
     cardHolderNameInputLabel = "Name",
 }) => {
+    const [{ value }, , { setValue }] = useField("cardHolderName");
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        const value = event.target.value;
+
+        if (value.match(onlyLetters)) setValue(value.toUpperCase());
+    };
+
     return (
         <div className="form-group name-group">
             <label htmlFor="cardHolderName">{cardHolderNameInputLabel}</label>
@@ -15,6 +25,8 @@ const CardHolderNameInput: FC<CardHolderNameInputType> = ({
                 type="text"
                 id="cardHolderName"
                 required
+                value={value}
+                onChange={onChange}
             />
         </div>
     );
--- src/utils/patterns.ts
+++ src/utils/patterns.ts
@@ -1,3 +1,4 @@
 export const fourDigits = /.{1,4}/g,
     threeDigits = /.{1,3}/g,
-    onlyDigits = /^[0-9]*$/;
+    onlyDigits = /^[0-9]*$/,
+    onlyLetters = /^[A-Za-z ]*$/;
